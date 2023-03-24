4e755527a3d206727bc081e6e2ac5d4ecaf250d6
RicarTang <357965604@qq.com>
2023-03-24 16:42:03 +0800

    修改

--- pydantic_validator.py
+++ pydantic_validator.py
@@ -2,18 +2,24 @@ from pydantic import BaseModel,Field
 from typing import Optional
 
 class Curtime(BaseModel):
+    """定义pydantic模型"""
     curtime: Optional[int] = None
 
 class User(BaseModel):
     name: str = Field(min_length=2)
-    age: int = Field(ge=80)
+    age: int = Field(ge=0)
 
 def validator(model):
+    """定义验证装饰器
+
+    Args:
+        model (PydanticModel): 带参数的装饰器，传入pydantic模型
+    """
     def decorate(func):
         def wrapper(data):
             try:
-                time = model(**data)
-                return func(time)
+                pydantic_obj = model(**data)
+                return func(pydantic_obj)
             except ValueError as e:
                 print(e.json())
                 return None
@@ -29,7 +35,7 @@ def user(user: User):
     print(user)
 
 # t_time({"curtime":1})
-user({"name":"t","age":81})
+user({"name":"tang","age":9})
 
 
 
